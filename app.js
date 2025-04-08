55ea86b8a9a05202e953c031108f25ac26b1fd15
senthilmilitant <senthilmilitant@gmail.com>
2025-04-08 17:43:37 +0530

    connected to data base

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import mongoose from 'mongoose'
 
 const app = express()
 const PORT = 3000
@@ -10,3 +11,12 @@ app.get('/', (req, res) => {
 app.listen(PORT,()=>{
     console.log(`This server is running on port ${PORT}`)
 })
+
+
+
+
+mongoose.connect('mongodb+srv://senthilmilitant:xODEvzoW1FDNPncm@cluster0.5ephglg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => console.log('Connected!'))
+  .catch(()=>{
+    console.log(`connection failed!`)
+  })
